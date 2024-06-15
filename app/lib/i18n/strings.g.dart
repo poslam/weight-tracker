69c74e128df25b0e431c7444688ef2c7acfd3a19
poslam <kirden04@gmail.com>
2024-06-15 12:11:39 +1000

    windows

--- app/lib/i18n/strings.g.dart
+++ app/lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 60 (30 per locale)
 ///
-/// Built on 2024-06-15 at 01:33 UTC
+/// Built on 2024-06-15 at 02:11 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
